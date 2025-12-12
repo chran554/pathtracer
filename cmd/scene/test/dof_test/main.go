ca2645c72c8c13a3a1d1b9b4a4463fcfa409940d
christian <chran554@hotmail.com>
2025-12-12 09:48:54 +0100

    add test scene for depth of field (dof)

--- cmd/scene/test/dof_test/main.go
+++ cmd/scene/test/dof_test/main.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"fmt"
+	"math"
 	"pathtracer/internal/pkg/color"
+	"pathtracer/internal/pkg/floatimage"
 	"pathtracer/internal/pkg/obj"
 	"pathtracer/internal/pkg/renderfile"
 	"pathtracer/internal/pkg/scene"
@@ -13,52 +15,95 @@ import (
 
 var animationName = "dof_test"
 
+var amountFrames = 4 * 24
+
 var ballRadius float64 = 30
 
-var amountSamples = 128 * 8 * 8 * 3
-var lensRadius = 0.0 // 12.0
+var amountSamples = 1024 * 4 // 128 * 8 * 8 * 3
+var maxLensRadius = 48.0     // 12.0
 
 var viewPlaneDistance = 2000.0
 var cameraDistanceFactor = 1.0
 
-var imageWidth = 800
-var imageHeight = 400
-var magnification = 1.0
+var imageWidth = 1200
+var imageHeight = 600
+var magnification = 0.5
 
 func main() {
-	cornellBox := obj.NewCornellBox(&vec3.T{500, 300, 500}, false, 7.0) // cm, as units. I.e. a 5x3x5m room
+	wallTexture, err := floatimage.EmptyPlaceholderImage("textures/wallpaper/anemone-rose-flower-eucalyptus-leaves-pampas-grass.png")
+	if err != nil {
+		panic(err)
+	}
 
-	amountSpheres := 5
+	floorTexture, err := floatimage.EmptyPlaceholderImage("textures/floor/tilesf4.jpeg")
+	if err != nil {
+		panic(err)
+	}
+
+	cornellBox := obj.NewCornellBox(&vec3.T{600, 300, 600}, false, 4.0) // cm, as units. I.e., a 5x3x5m room
+	cornellBoxUnit := cornellBox.Bounds.SizeY() / 2
+
+	leftWall := cornellBox.GetFirstObjectByMaterialName("Left")
+	leftWall.Material = scene.NewMaterial().N("Left").PP(wallTexture, &vec3.T{leftWall.Bounds.Xmin, leftWall.Bounds.Ymin, leftWall.Bounds.Zmin}, vec3.UnitZ.Scaled(cornellBoxUnit), vec3.UnitY.Scaled(cornellBoxUnit))
+
+	rightWall := cornellBox.GetFirstObjectByMaterialName("Right")
+	rightWall.Material = scene.NewMaterial().N("Right").PP(wallTexture, &vec3.T{rightWall.Bounds.Xmax, rightWall.Bounds.Ymin, rightWall.Bounds.Zmax}, vec3.UnitZ.Scaled(-cornellBoxUnit), vec3.UnitY.Scaled(cornellBoxUnit))
+
+	backWall := cornellBox.GetFirstObjectByMaterialName("Back")
+	backWall.Material = scene.NewMaterial().N("Back").PP(wallTexture, &vec3.T{backWall.Bounds.Xmin, backWall.Bounds.Ymin, backWall.Bounds.Zmax}, vec3.UnitX.Scaled(cornellBoxUnit), vec3.UnitY.Scaled(cornellBoxUnit))
+
+	floor := cornellBox.GetFirstObjectByMaterialName("Floor")
+	floor.Material = scene.NewMaterial().N("Floor").PP(floorTexture, &vec3.T{floor.Bounds.Xmin, floor.Bounds.Ymin, floor.Bounds.Zmin}, vec3.UnitX.Scaled(cornellBoxUnit/2), vec3.UnitZ.Scaled(cornellBoxUnit/2))
+
+	amountSpheres := 7
 	sphereSpread := ballRadius * 2.0 * (float64(amountSpheres) + 1)
-	sphereCC := sphereSpread / float64(amountSpheres)
+	sphereCC := sphereSpread / float64(amountSpheres-1)
 
-	sphereMaterial := scene.NewMaterial().C(color.NewColor(0.85, 0.95, 0.80)).M(0.4, 0.05)
+	sphereMaterial := scene.NewMaterial().
+		C(color.NewColor(0.95, 0.75, 0.70)).
+		M(0.3, 0.0).
+		T(0.0, true, scene.RefractionIndex_AcrylicPlastic)
 
 	var spheres []*scene.Sphere
-	for i := 0; i <= amountSpheres; i++ {
-		positionOffsetX := (-sphereSpread/2.0 + float64(i)*sphereCC) * 0.5
-		positionOffsetZ := (-sphereSpread/2.0 + float64(i)*sphereCC) * 1.0
+	for i := 0; i < amountSpheres; i++ {
+		positionOffsetX := (-sphereSpread/2.0 + float64(i)*sphereCC) * 0.50
+		positionOffsetZ := (-sphereSpread/2.0 + float64(i)*sphereCC) * 0.75
 
-		sphere := scene.NewSphere(&vec3.T{positionOffsetX, ballRadius, positionOffsetZ}, ballRadius, sphereMaterial).
-			N("Glass sphere with transparency " + strconv.Itoa(i))
+		sphereOrigin := &vec3.T{positionOffsetX, ballRadius, positionOffsetZ}
+		sphere := scene.NewSphere(sphereOrigin, ballRadius, sphereMaterial).N("Sphere" + strconv.Itoa(i))
 
 		spheres = append(spheres, sphere)
 	}
 
 	scn := scene.NewSceneNode().S(spheres...).FS(cornellBox)
 
-	cameraOrigin := vec3.T{0, ballRadius * 3, -800}
+	cameraOrigin := vec3.T{0, 200, -800}
 	cameraOrigin.Scale(cameraDistanceFactor)
-	focusPoint := vec3.T{0, ballRadius, 0}
-	camera := scene.NewCamera(&cameraOrigin, &focusPoint, amountSamples, magnification).V(viewPlaneDistance).A(lensRadius, nil)
-
-	frame := scene.NewFrame(animationName, -1, camera, scn)
+	focusPoint := vec3.T{0, ballRadius, -ballRadius}
 
 	animation := scene.NewAnimation(animationName, imageWidth, imageHeight, magnification, false, false)
-	animation.AddFrame(frame)
+
+	for frameIndex := 0; frameIndex < amountFrames; frameIndex++ {
+		animationProgress := float64(frameIndex) / float64(amountFrames)
+
+		lensRadius := maxLensRadius * (0.5 + math.Sin(-math.Pi/2+math.Pi*animationProgress)*0.5) // Slow start and slow end
+
+		camera := scene.NewCamera(&cameraOrigin, &focusPoint, amountSamples, magnification).
+			V(viewPlaneDistance).
+			A(lensRadius, nil).
+			D(6)
+
+		fi := -1
+		if amountFrames > 1 {
+			fi = frameIndex
+		}
+
+		frame := scene.NewFrame(animationName, fi, camera, scn)
+		animation.AddFrame(frame)
+	}
 
 	filename := fmt.Sprintf("scene/%s.render.zip", animation.AnimationName)
-	err := renderfile.WriteRenderFile(filename, animation)
+	err = renderfile.WriteRenderFile(filename, animation)
 	if err != nil {
 		panic(err)
 	}
